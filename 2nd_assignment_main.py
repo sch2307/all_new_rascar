5533ba3e668b7160942de0eab8e9ca8906d15863
ChoiJuwon <cjw980221@kookmin.ac.kr>
2018-10-24 10:28:53 +0900

    Update 2nd_assignment_main.py

---
+++ 2nd_assignment_main.py
@@ -0,0 +1,38 @@
+#########################################################################
+# Date: 2018/10/02
+# file name: 2nd_assignment_main.py
+# Purpose: this code has been generated for the 4 wheel drive body
+# moving object to perform the project with line detector
+# this code is used for the student only
+#########################################################################
+
+from car import Car
+import time
+
+
+class myCar(object):
+
+    def __init__(self, car_name):
+        self.car = Car(car_name)
+
+    def drive_parking(self):
+        self.car.drive_parking()
+
+    # =======================================================================
+    # 2ND_ASSIGNMENT_CODE
+    # Complete the code to perform Second Assignment
+    # =======================================================================
+    def car_startup(self):
+        # implement the assignment code here
+        pass
+
+
+if __name__ == "__main__":
+    try:
+        myCar = myCar("CarName")
+        myCar.car_startup()
+
+    except KeyboardInterrupt:
+        # when the Ctrl+C key has been pressed,
+        # the moving object will be stopped
+        myCar.drive_parking()
